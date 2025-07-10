0c6297274d6d4dcdd5f7ecb7305af80161a6fb80
Pedro Manuel <masped574@gmail.com>
2025-07-10 11:24:05 +0700

    Simplify face_preprocessing to fix ImportError

--- face_preprocessing.py
+++ face_preprocessing.py
@@ -2,7 +2,6 @@
 
 import cv2
 from deepface import DeepFace
-from deepface.commons import a_folder, a_file
 import numpy as np
 
 def detect_and_crop(img_path):
@@ -12,13 +11,14 @@ def detect_and_crop(img_path):
     Returns None if no face is detected.
     """
     try:
-        # DeepFace.detectFace akan mendeteksi dan meng-crop wajah.
+        # DeepFace.extract_faces akan mendeteksi dan meng-crop wajah.
         # Backend 'mtcnn' digunakan secara default.
-        # a_folder untuk menghapus direktori hasil ekstraksi wajah dari deepface
-        face_obj = DeepFace.extract_faces(img_path=img_path, 
-                                          enforce_detection=True, 
-                                          detector_backend='mtcnn',
-                                          align=True)
+        face_obj = DeepFace.extract_faces(
+            img_path=img_path, 
+            enforce_detection=True, 
+            detector_backend='mtcnn',
+            align=True
+        )
         
         # Ambil data gambar dari hasil
         face_img = face_obj[0]['face'] # Ambil wajah pertama yang terdeteksi
@@ -29,10 +29,8 @@ def detect_and_crop(img_path):
         # Konversi ke Grayscale
         gray_face = cv2.cvtColor(face_img_uint8, cv2.COLOR_RGB2GRAY)
         
-        # hapus folder hasil ekstraksi dari deepface
-        a_folder.delete_folder_if_exist(f"tmp_{a_file.get_file_name(img_path)}")
-        
         return gray_face
+        
     except Exception as e:
         print(f"Wajah tidak terdeteksi atau error di DeepFace: {e}")
         return None
